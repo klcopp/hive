7c102a1c1dc3f0c2ec26d56bb81ab8d79510ea94
Karen Coppage <karen.coppage@cloudera.com>
2019-05-10 10:43:13 +0200

    dtf - Thursday - FROM COMMIT

--- common/src/test/org/apache/hadoop/hive/common/format/datetime/TestHiveSqlDateTimeFormatter.java
+++ common/src/test/org/apache/hadoop/hive/common/format/datetime/TestHiveSqlDateTimeFormatter.java
@@ -20,8 +20,11 @@ package org.apache.hadoop.hive.common.format.datetime;
 
 import com.sun.tools.javac.util.List;
 import junit.framework.TestCase;
+import org.apache.hadoop.hive.common.type.Timestamp;
 import org.junit.Test;
 
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalField;
 import java.util.ArrayList;
 
 /**
@@ -34,31 +37,31 @@ public class TestHiveSqlDateTimeFormatter extends TestCase {
 
   @Test
   public void testSetPattern() throws IllegalArgumentException{
-    verifyPatternParsing(" ---yyyy-\'-:-  -,.;/MM-dd--", 27, new ArrayList<>(List.of(
-        HiveSqlDateTimeFormatter.TokenType.SEPARATOR,
-        HiveSqlDateTimeFormatter.TokenType.YEAR,
-        HiveSqlDateTimeFormatter.TokenType.SEPARATOR,
-        HiveSqlDateTimeFormatter.TokenType.MONTH,
-        HiveSqlDateTimeFormatter.TokenType.SEPARATOR,
-        HiveSqlDateTimeFormatter.TokenType.DAY_OF_MONTH,
-        HiveSqlDateTimeFormatter.TokenType.SEPARATOR
+    verifyPatternParsing(" ---yyyy-\'-:-  -,.;/MM-dd--", new ArrayList<>(List.of(
+        null,
+        ChronoField.YEAR,
+        null,
+        ChronoField.MONTH_OF_YEAR,
+        null,
+        ChronoField.DAY_OF_MONTH,
+        null
         )));
 
-    verifyPatternParsing("yyyymmdd", 8, new ArrayList<>(List.of(
-        HiveSqlDateTimeFormatter.TokenType.YEAR,
-        HiveSqlDateTimeFormatter.TokenType.MONTH,
-        HiveSqlDateTimeFormatter.TokenType.DAY_OF_MONTH
+    verifyPatternParsing("yyyymmdd", new ArrayList<>(List.of(
+        ChronoField.YEAR,
+        ChronoField.MONTH_OF_YEAR,
+        ChronoField.DAY_OF_MONTH
     )));
 
-    verifyPatternParsing("hh:mi:ss", 8, new ArrayList<>(List.of(
-        HiveSqlDateTimeFormatter.TokenType.HOUR_IN_HALF_DAY,
-        HiveSqlDateTimeFormatter.TokenType.SEPARATOR,
-        HiveSqlDateTimeFormatter.TokenType.MINUTE,
-        HiveSqlDateTimeFormatter.TokenType.SEPARATOR,
-        HiveSqlDateTimeFormatter.TokenType.SECOND
+    verifyPatternParsing("hh24::mi:ss", new ArrayList<>(List.of(
+        ChronoField.HOUR_OF_DAY,
+        null,
+        ChronoField.MINUTE_OF_HOUR,
+        null,
+        ChronoField.SECOND_OF_MINUTE
     )));
-    verifyPatternParsing("y", 1, new ArrayList<>(List.of(
-        HiveSqlDateTimeFormatter.TokenType.YEAR
+    verifyPatternParsing("y", new ArrayList<>(List.of(
+        ChronoField.YEAR
     )));
   }
 
@@ -77,26 +80,45 @@ public class TestHiveSqlDateTimeFormatter extends TestCase {
     }
   }
 
-  private void verifyPatternParsing(String pattern, int expectedLength, ArrayList<HiveSqlDateTimeFormatter.TokenType> expected)
+  private void verifyPatternParsing(String pattern, ArrayList<TemporalField> expected)
       throws IllegalArgumentException {
     formatter.setPattern(pattern, true);
     assertEquals(expected.size(), formatter.tokens.size());
     StringBuilder sb = new StringBuilder();
-    int actualLength = 0;
+    int actualPatternLength = 0;
     for (int i = 0; i < expected.size(); i++) {
-      assertEquals("Generated list of tokens not correct", expected.get(i), formatter.tokens.get(i).type);
+      assertEquals("Generated list of tokens not correct", expected.get(i), formatter.tokens.get(i).temporalField);
       sb.append(formatter.tokens.get(i).string);
-      actualLength += formatter.tokens.get(i).length;
+      actualPatternLength += formatter.tokens.get(i).length;
     }
     assertEquals("Token strings concatenated don't match original pattern string", pattern.toLowerCase(), sb.toString());
-    assertEquals(expectedLength, actualLength);
+    assertEquals(pattern.length(), actualPatternLength);
   }
 
   @Test
   public void testFormat() {
+    formatter.setPattern("yyyy", false);
+    Timestamp ts = Timestamp.valueOf("2018-01-01 00:00:00");
+    assertEquals("2018", formatter.format(ts));
+
+    formatter.setPattern("yyyy-mm-dd", false);
+    ts = Timestamp.valueOf("2018-02-03 00:00:00");
+    assertEquals("2018-02-03", formatter.format(ts));
+
+    formatter.setPattern("hh24:mi:ss", false);
+    ts = Timestamp.valueOf("2018-02-03 01:02:03");
+    assertEquals("01:02:03", formatter.format(ts));
   }
 
   @Test
   public void testParse() throws ParseException {
+    formatter.setPattern("yy-mm-dd hh24:mi:ss", true);
+    assertEquals(Timestamp.valueOf("2018-02-03 04:05:06"), formatter.parse("18-02-03 04:05:06"));
+
+    formatter.setPattern("yyyy-mm-dd", true);
+    assertEquals(Timestamp.valueOf("2018-01-01 00:00:00"), formatter.parse("2018-01-01"));
+
+    formatter.setPattern("yyyy", true);
+    assertEquals(Timestamp.valueOf("2018-01-01 00:00:00"), formatter.parse("2018"));
   }
 }
